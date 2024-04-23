f73f4dc375477446914b95fe7190b46b339550a1
Joslynn20 <joslynnkim@gmail.com>
2024-04-23 20:24:55 +0900

    feat: Posts, Comments 일급 컬렉션 추가

--- src/main/java/com/sns/whisper/domain/post/domain/Posts.java
+++ src/main/java/com/sns/whisper/domain/post/domain/Posts.java
@@ -1,2 +1,21 @@
-package com.sns.whisper.domain.post.domain;public class Posts {
+package com.sns.whisper.domain.post.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Posts {
+
+    @OneToMany(
+            mappedBy = "user",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Post> posts = new ArrayList<>();
+
+    protected Posts() {
+    }
 }
