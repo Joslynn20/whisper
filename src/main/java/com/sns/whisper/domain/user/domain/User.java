67b2f49a7fd19b5c3b61e82737c38acb54f22cb1
Joslynn20 <joslynnkim@gmail.com>
2024-04-23 18:06:05 +0900

    feat: delete 정보 추가
    사용자의 탈퇴 여부 관련 정보들을 DeleteInfo 클래스로 담은 후
    Embedded 활용

--- src/main/java/com/sns/whisper/domain/user/domain/User.java
+++ src/main/java/com/sns/whisper/domain/user/domain/User.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "\"user\"")
@@ -30,9 +31,55 @@ public class User extends BaseEntity {
     protected User() {
     }
 
-    public User(Long no, BasicProfile basicProfile, DeleteInfo deleteInfo) {
-        this.no = no;
+    public User(BasicProfile basicProfile, DeleteInfo deleteInfo) {
+        this.no = null;
         this.basicProfile = basicProfile;
         this.deleteInfo = deleteInfo;
+    }
+
+    public Long getNo() {
+        return no;
+    }
+
+    public String getId() {
+        return basicProfile.getId();
+    }
+
+    public String getPassword() {
+        return basicProfile.getPassword();
+    }
+
+    public String getEmail() {
+        return basicProfile.getEmail();
+    }
+
+    public String getProfileImage() {
+        return basicProfile.getProfileImage();
+    }
+
+    public String getProfileMessage() {
+        return basicProfile.getProfileMessage();
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNo());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof User)) {
+            return false;
+        }
+
+        User user = (User) obj;
+
+        return no != null ? no.equals(user
+                .getNo()) : user.getNo() == null;
     }
 }
--- src/main/java/com/sns/whisper/domain/user/domain/profile/DeleteInfo.java
+++ src/main/java/com/sns/whisper/domain/user/domain/profile/DeleteInfo.java
@@ -27,4 +27,5 @@ public class DeleteInfo {
     public LocalDateTime getDeleteAt() {
         return deleteAt;
     }
+    
 }
