0639bcec3970c7200d04d230ef99cd1218dba307
Joslynn20 <joslynnkim@gmail.com>
2024-04-24 21:39:14 +0900

    feat: 회원가입 기능 개발
    1. 회원가입 시 최초 회원 상태는 PENDING으로 설정
    2. 해피케이스 테스트코드 작성

---
+++ src/main/java/com/sns/whisper/domain/user/domain/respository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sns.whisper.domain.user.domain.respository;
+
+import com.sns.whisper.domain.user.domain.User;
+
+public interface UserRepository {
+
+    User save(User user);
+}
--- src/main/java/com/sns/whisper/domain/user/infrastructure/S3ProfileStorage.java
+++ src/main/java/com/sns/whisper/domain/user/infrastructure/S3ProfileStorage.java
@@ -1,8 +1,10 @@
 package com.sns.whisper.domain.user.infrastructure;
 
 import com.sns.whisper.domain.user.domain.respository.ProfileStorage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Profile("!test")
@@ -11,12 +13,15 @@ public class S3ProfileStorage implements ProfileStorage {
 
     private final S3Client s3Client;
 
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
     public S3ProfileStorage(S3Client s3Client) {
         this.s3Client = s3Client;
     }
 
     @Override
-    public String store(byte[] image) {
+    public String store(MultipartFile image) {
         return null;
     }
 }
