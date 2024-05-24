96c9288fd91ac0f4177396f805f4fd31a0e830c3
Joslynn20 <joslynnkim@gmail.com>
2024-05-24 19:16:57 +0900

    test: 중복된 아이디 입력 시, 회원가입에 실패하는 테스트 추가

--- src/test/java/com/sns/whisper/spring/integration/user/GeneralUserServiceIntegrationTest.java
+++ src/test/java/com/sns/whisper/spring/integration/user/GeneralUserServiceIntegrationTest.java
@@ -2,12 +2,15 @@ package com.sns.whisper.spring.integration.user;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.sns.whisper.domain.user.application.UserService;
 import com.sns.whisper.domain.user.application.dto.request.UserCreateRequest;
 import com.sns.whisper.domain.user.application.dto.response.UserResponse;
+import com.sns.whisper.domain.user.domain.User;
 import com.sns.whisper.domain.user.domain.profile.UserStatus;
 import com.sns.whisper.domain.user.infrastructure.JPAUserRepository;
+import com.sns.whisper.exception.user.DuplicatedUserIdException;
 import com.sns.whisper.spring.integration.IntegrationTest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 public class GeneralUserServiceIntegrationTest extends IntegrationTest {
 
@@ -55,5 +59,31 @@ public class GeneralUserServiceIntegrationTest extends IntegrationTest {
         assertThat(userResponse.getProfileImage()).contains("basic_profile.png");
     }
 
+    @Test
+    @DisplayName("중복된 아이디는 회원 가입에 실패한다.")
+    void signUp_DuplicatedUserId_ThrownException() throws Exception {
+        //given
+        String userId = "testId1234";
 
+        User formerUser = User.create(userId, "password1234", "user@gmail.com",
+                LocalDate.of(1996, 4, 24), "basic_profile.png", "프로필 메세지", LocalDateTime.now());
+
+        userRepository.save(formerUser);
+
+        UserCreateRequest request = UserCreateRequest.builder()
+                                                     .userId(userId)
+                                                     .password("userPassword123")
+                                                     .email("test@gmail.com")
+                                                     .birth(LocalDate.of(1996, 4, 24))
+                                                     .profileImage(null)
+                                                     .profileMessage("회원 메세지")
+                                                     .joinedAt(LocalDateTime.now())
+                                                     .build();
+        //when, then
+        assertThatCode(() -> userService.signUp(request)).isInstanceOf(
+                                                                 DuplicatedUserIdException.class)
+                                                         .hasFieldOrPropertyWithValue("httpStatus",
+                                                                 HttpStatus.BAD_REQUEST)
+                                                         .hasMessage("중복된 아이디입니다.");
+    }
 }
