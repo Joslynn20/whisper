91f459eef47b763689613e2b7fe5d5648fe3e5f6
Joslynn20 <joslynnkim@gmail.com>
2024-05-24 21:03:38 +0900

    remove: UserService 인터페이스 삭제

--- src/main/java/com/sns/whisper/domain/user/application/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.sns.whisper.domain.user.application;
-
-import com.sns.whisper.domain.user.application.dto.request.UserCreateRequest;
-import com.sns.whisper.domain.user.application.dto.response.UserResponse;
-
-public interface UserService {
-
-    UserResponse signUp(UserCreateRequest request);
-}
