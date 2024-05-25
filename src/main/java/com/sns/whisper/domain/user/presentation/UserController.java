90d83f39508c79fb17ad9b0377a7626497c212f8
Joslynn20 <joslynnkim@gmail.com>
2024-05-25 22:17:54 +0900

    feat: 로그인 controller 로직 작성 및 테스트

--- src/main/java/com/sns/whisper/domain/user/presentation/UserController.java
+++ src/main/java/com/sns/whisper/domain/user/presentation/UserController.java
@@ -4,6 +4,7 @@ import com.sns.whisper.domain.user.application.GeneralUserService;
 import com.sns.whisper.domain.user.presentation.request.UserSignUpRequest;
 import com.sns.whisper.global.dto.HttpResponseDto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,11 +22,17 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> signUp(@Valid UserSignUpRequest request) {
-        
+
         LocalDateTime joinedAt = LocalDateTime.now();
         userService.signUp(request.toServiceRequest(joinedAt));
 
         return HttpResponseDto.ok(HttpStatus.CREATED, "회원가입에 성공했습니다.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@NotBlank String userId, @NotBlank String password) {
+        userService.login(userId, password);
+        return HttpResponseDto.ok(HttpStatus.OK, "로그인되었습니다.");
     }
 
 }
