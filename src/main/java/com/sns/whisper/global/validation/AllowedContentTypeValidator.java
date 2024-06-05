fb967c70daa64ff68b8725170ca07a99616bf80f
Joslynn20 <joslynnkim@gmail.com>
2024-06-05 20:36:22 +0900

    rename: global로 이동

--- src/main/java/com/sns/whisper/global/validation/AllowedContentTypeValidator.java
+++ src/main/java/com/sns/whisper/global/validation/AllowedContentTypeValidator.java
@@ -1,4 +1,4 @@
-package com.sns.whisper.global.annotation.validation;
+package com.sns.whisper.global.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
