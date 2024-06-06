6b5228b9fddc219eadb581d1fe3c6f69b2aac6f7
Joslynn20 <joslynnkim@gmail.com>
2024-06-06 21:28:45 +0900

    feat: JpaConfiguration에 JPAPostRepository 클래수 추가

--- src/main/java/com/sns/whisper/global/config/JpaConfiguration.java
+++ src/main/java/com/sns/whisper/global/config/JpaConfiguration.java
@@ -1,11 +1,12 @@
 package com.sns.whisper.global.config;
 
+import com.sns.whisper.domain.post.infrastructure.JPAPostRepository;
 import com.sns.whisper.domain.user.infrastructure.JPAUserRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackageClasses = {JPAUserRepository.class})
+@EnableJpaRepositories(basePackageClasses = {JPAUserRepository.class, JPAPostRepository.class})
 @EnableJpaAuditing
 @Configuration
 public class JpaConfiguration {
