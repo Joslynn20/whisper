55241e0a63d0ca615d3e21e5569cb1ab5a837d42
Joslynn20 <joslynnkim@gmail.com>
2024-06-06 21:25:12 +0900

    refactor: PostContent 클래스 삭제

--- src/main/java/com/sns/whisper/domain/post/domain/Post.java
+++ src/main/java/com/sns/whisper/domain/post/domain/Post.java
@@ -1,8 +1,8 @@
 package com.sns.whisper.domain.post.domain;
 
 import com.sns.whisper.domain.post.domain.content.Images;
-import com.sns.whisper.domain.post.domain.content.PostContent;
 import com.sns.whisper.domain.user.domain.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,9 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.Builder;
 
 @Entity
 @Table(name = "post")
@@ -28,28 +26,21 @@ public class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Embedded
-    private PostContent content;
+    @Column(columnDefinition = "TEXT")
+    private String content;
 
     @Embedded
     private Images images;
 
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
 
     protected Post() {
     }
 
-    public Post(Long id, User user, PostContent content, Images images, LocalDateTime createdAt,
-            LocalDateTime modifiedAt) {
+    @Builder
+    private Post(Long id, User user, String content, Images images) {
         this.id = id;
         this.user = user;
         this.content = content;
         this.images = images;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
     }
 }
