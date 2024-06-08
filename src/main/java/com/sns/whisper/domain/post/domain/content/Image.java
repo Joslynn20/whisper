6e1df1b388170f458b04d18ed3da034be06ebd9a
Joslynn20 <joslynnkim@gmail.com>
2024-06-08 20:18:23 +0900

    feat: Post 도메인 설계, Entity 작성 1. Post가 Images 일급 객체를 필드로 가지도록 함 2. Image에는 belongTo 메소드가 있어 소속된 Post 객체에 소속되도록 함

--- src/main/java/com/sns/whisper/domain/post/domain/content/Image.java
+++ src/main/java/com/sns/whisper/domain/post/domain/content/Image.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.util.Objects;
 
 @Entity
 public class Image {
@@ -24,5 +25,36 @@ public class Image {
     @Column(nullable = false)
     private String url;
 
+    protected Image() {
+    }
 
+    public Image(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void belongTo(Post post) {
+        this.post = post;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Image image = (Image) o;
+        return Objects.equals(url, image.getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
 }
