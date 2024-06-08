c02725191f7f02e53b71fd82a81b456cf0be51e9
Joslynn20 <joslynnkim@gmail.com>
2024-06-08 19:13:29 +0900

    refactor: FileUtil 공통 로직 수정 1. User Domain의 ProfileStorge의 파일 저장 로직과 Post Domain의 파일 이름 생성 로직이 겹치는 구간 발생 2. FileUtil을 수정하여 파일 이름을 생성하는 공통 로직 수행하도록 함

---
+++ src/main/java/com/sns/whisper/domain/post/domain/repository/ImageStorage.java
@@ -0,0 +1,9 @@
+package com.sns.whisper.domain.post.domain.repository;
+
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageStorage {
+
+    List<String> storeImages(List<MultipartFile> images, String userId);
+}
