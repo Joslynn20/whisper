4019b801979f175c006dacf514f0165fa9e2ba8b
Joslynn20 <joslynnkim@gmail.com>
2024-07-03 21:15:32 +0900

    feat: postId를 통해 Post 엔티티 조회 로직 작성

--- src/main/java/com/sns/whisper/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/sns/whisper/domain/post/domain/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.sns.whisper.domain.post.domain.repository;
 
 import com.sns.whisper.domain.post.domain.Post;
+import java.util.Optional;
 
 public interface PostRepository {
 
     Post save(Post post);
-    
+
+    Optional<Post> findById(Long id);
+
 }
