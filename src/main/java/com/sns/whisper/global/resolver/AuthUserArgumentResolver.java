533d791922f65b17f5e897423a449bd42065562a
Joslynn20 <joslynnkim@gmail.com>
2024-07-05 20:26:43 +0900

    feat: Custom Argument Resolver 적용하기 1. 커스텀 어노테이션과 Argument Resolver를 적용하여 로그인된 회원의 아이디 정보를 가져오는 로직을 Controller에서 분리 및 중복 코드 제거 2. WebMvcConfiguration 설정을 통해 커스텀한 AuthUserArgumentResolver 설정을 추가

---
+++ src/main/java/com/sns/whisper/global/resolver/AuthUserArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.sns.whisper.global.resolver;
+
+import com.sns.whisper.domain.user.application.LoginService;
+import com.sns.whisper.exception.post.NotAuthorizedUserException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final LoginService loginService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        String userId = loginService.getCurrentUserId();
+
+        if (userId == null) {
+            throw new NotAuthorizedUserException();
+        }
+
+        return new AuthUser(userId);
+    }
+}
