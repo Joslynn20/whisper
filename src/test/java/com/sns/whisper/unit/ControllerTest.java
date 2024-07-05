df7a1f302b50a4f3d11b01721c22a0f5984a5597
Joslynn20 <joslynnkim@gmail.com>
2024-07-05 20:32:08 +0900

    test: MockAuthUserArgumentResolver 클래스를 적용하여 테스트 모듈 분리 1. AuthUserArgumentResolver 대신 MockAuthUserArgumentResolver를 테스트 환경에 적용하여 로그인 이후의 로직을 검증할 수 있도록 함 2. WebTestConfiguration 설정으로 테스트 환경에서 커스텀 Argument Resolver 적용 3. Controller 테스트에서 LoginCheck AOP 적용을 위한 AOP Configuration 및 LoginCheckAspect 적용 4. Controller 테스트에서 MockAuthUserArgumentResolver가 적용될 수 있도록 Configuration 적용

--- src/test/java/com/sns/whisper/unit/ControllerTest.java
+++ src/test/java/com/sns/whisper/unit/ControllerTest.java
@@ -1,18 +1,33 @@
 package com.sns.whisper.unit;
 
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sns.whisper.config.InfrastructureTestConfiguration;
+import com.sns.whisper.config.WebTestConfiguration;
 import com.sns.whisper.domain.post.application.PostService;
 import com.sns.whisper.domain.post.presentation.PostController;
 import com.sns.whisper.domain.user.application.LoginService;
 import com.sns.whisper.domain.user.application.UserService;
 import com.sns.whisper.domain.user.presentation.UserController;
+import com.sns.whisper.global.aop.LoginCheckAspect;
+import com.sns.whisper.global.config.WebMvcConfiguration;
+import com.sns.whisper.global.resolver.AuthUserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest({UserController.class, PostController.class})
+@WebMvcTest(
+        value = {UserController.class, PostController.class},
+        excludeFilters = {@Filter(type = ASSIGNABLE_TYPE, classes = WebMvcConfiguration.class)}
+)
+@Import({AopAutoConfiguration.class, LoginCheckAspect.class,
+        InfrastructureTestConfiguration.class, WebTestConfiguration.class})
 @ActiveProfiles("test")
 public class ControllerTest {
 
@@ -24,6 +39,9 @@ public class ControllerTest {
 
     @MockBean
     protected PostService postService;
+
+    @MockBean
+    protected AuthUserArgumentResolver authUserArgumentResolver;
 
     @Autowired
     protected MockMvc mockMvc;
