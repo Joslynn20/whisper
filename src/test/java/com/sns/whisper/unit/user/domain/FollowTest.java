4380a2d1f2c3a86b61607d13d840ee926dd4c782
Joslynn20 <joslynnkim@gmail.com>
2024-07-12 19:29:20 +0900

    rename: 파일명 변경

--- src/test/java/com/sns/whisper/unit/user/domain/FollowTest.java
+++ src/test/java/com/sns/whisper/unit/user/domain/FollowTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-public class FollowingTest {
+public class FollowTest {
 
     @Test
     @DisplayName("회원은 자기 자신을 팔로우할 수 없다.")
