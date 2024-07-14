b5dfcb578c7fa9cdd922d670d7b8898b6bd4d9e9
Joslynn20 <joslynnkim@gmail.com>
2024-07-14 19:52:35 +0900

    rename: NotValidUserException -> InvalidUserException으로 클래스명 변경

--- src/main/java/com/sns/whisper/exception/user/InvalidUserException.java
+++ src/main/java/com/sns/whisper/exception/user/InvalidUserException.java
@@ -3,12 +3,12 @@ package com.sns.whisper.exception.user;
 import com.sns.whisper.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
-public class NotValidUserException extends BusinessException {
+public class InvalidUserException extends BusinessException {
 
     private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
     private static final String MESSAGE = "유효하지 않은 회원입니다.";
 
-    public NotValidUserException() {
+    public InvalidUserException() {
         super(HTTP_STATUS, MESSAGE);
     }
 
