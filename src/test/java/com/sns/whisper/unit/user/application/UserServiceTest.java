7b4d9b5c14bc01da5b9fe1b2ccf45c61e8878243
Joslynn20 <joslynnkim@gmail.com>
2024-07-14 20:20:20 +0900

    feat: follow 기능 Service 로직 작성 및 테스트 1. 성공 및 실패 케이스 테스트 -> Nested 구조로 테스트 코드 가독성을 올림

--- src/test/java/com/sns/whisper/unit/user/application/UserServiceTest.java
+++ src/test/java/com/sns/whisper/unit/user/application/UserServiceTest.java
@@ -8,8 +8,11 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.sns.whisper.common.factory.UserFactory;
 import com.sns.whisper.domain.user.application.UserService;
+import com.sns.whisper.domain.user.application.dto.request.FollowServiceRequest;
 import com.sns.whisper.domain.user.application.dto.request.UserSignUpServiceRequest;
+import com.sns.whisper.domain.user.application.dto.response.FollowServiceResponse;
 import com.sns.whisper.domain.user.application.dto.response.UserResponse;
 import com.sns.whisper.domain.user.domain.User;
 import com.sns.whisper.domain.user.domain.profile.BasicProfile;
@@ -17,12 +20,16 @@ import com.sns.whisper.domain.user.domain.profile.Email;
 import com.sns.whisper.domain.user.domain.profile.UserStatus;
 import com.sns.whisper.domain.user.domain.respository.ProfileStorage;
 import com.sns.whisper.domain.user.domain.respository.UserRepository;
+import com.sns.whisper.exception.user.DuplicatedFollowException;
 import com.sns.whisper.exception.user.DuplicatedUserIdException;
+import com.sns.whisper.exception.user.InvalidUserException;
 import com.sns.whisper.exception.user.NotValidEmailFormatException;
+import com.sns.whisper.exception.user.SameFromToUserException;
 import com.sns.whisper.global.common.PasswordEncryptor;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -100,6 +107,156 @@ public class UserServiceTest {
                 .isInstanceOf(DuplicatedUserIdException.class)
                 .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.BAD_REQUEST)
                 .hasMessage("중복된 아이디입니다.");
+    }
+
+    @DisplayName("followUser 메서드는")
+    @Nested
+    class Describe_followUser {
+
+        @DisplayName("로그인 하지 않은 회원은")
+        @Nested
+        class Context_NotLoginUser {
+
+            @Test
+            @DisplayName("팔로우할 수 없다.")
+            void followUser_NotLoginUser_400Exception() throws Exception {
+                //given
+                String fromUser = "testId1";
+                String toUser = "testId2";
+
+                FollowServiceRequest followServiceRequest = new FollowServiceRequest(fromUser,
+                        toUser);
+
+                //when, then
+                assertThatCode(() -> userService.followUser(followServiceRequest))
+                        .isInstanceOf(InvalidUserException.class)
+                        .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        @DisplayName("존재하지 않는 회원은")
+        @Nested
+        class Context_InvalidUser {
+
+            @Test
+            @DisplayName("팔로우할 수 없다.")
+            void followUser_InvalidUser_400Exception() throws Exception {
+                //given
+                String fromUser = "testId";
+                User loginUser = UserFactory.user(1L, "testId");
+
+                String invalidToUser = "testId1";
+
+                given(userRepository.findUserByUserId(fromUser)).willReturn(Optional.of(loginUser));
+                given(userRepository.findUserByUserId(invalidToUser)).willReturn(Optional.empty());
+
+                FollowServiceRequest followServiceRequest = new FollowServiceRequest(fromUser,
+                        invalidToUser);
+
+                //when, then
+                assertThatCode(() -> userService.followUser(followServiceRequest))
+                        .isInstanceOf(InvalidUserException.class)
+                        .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.BAD_REQUEST);
+
+                verify(userRepository, times(1)).findUserByUserId(fromUser);
+                verify(userRepository, times(1)).findUserByUserId(invalidToUser);
+            }
+        }
+
+        @DisplayName("fromUser와 toUser가 동일하다면")
+        @Nested
+        class Context_SameFromToUser {
+
+            @Test
+            @DisplayName("팔로우할 수 없다.")
+            void followUser_SameFromToUser_400Exception() throws Exception {
+                //given
+                String fromUser = "testId";
+                String toUser = "testId";
+
+                User from = UserFactory.user(1L, "testId");
+                User to = UserFactory.user(1L, "testId");
+
+                FollowServiceRequest followServiceRequest = new FollowServiceRequest(fromUser,
+                        toUser);
+
+                given(userRepository.findUserByUserId(fromUser)).willReturn(Optional.of(from));
+                given(userRepository.findUserByUserId(toUser)).willReturn(Optional.of(to));
+
+                //when, then
+                assertThatCode(() -> userService.followUser(followServiceRequest))
+                        .isInstanceOf(SameFromToUserException.class)
+                        .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.BAD_REQUEST);
+
+                verify(userRepository, times(2)).findUserByUserId(fromUser);
+            }
+
+            @DisplayName("회원이 팔로우하지 않은 회원은")
+            @Nested
+            class Context_NotFollowedUser {
+
+                @Test
+                @DisplayName("팔로우할 수 있다.")
+                void followUser_NotFollowedUser_Success() throws Exception {
+                    //given
+                    String fromUser = "testId";
+                    String toUser = "testId1";
+
+                    User from = UserFactory.user(1L, "testId");
+                    User to = UserFactory.user(2L, "testId1");
+
+                    FollowServiceRequest followServiceRequest = new FollowServiceRequest(fromUser,
+                            toUser);
+
+                    given(userRepository.findUserByUserId(fromUser)).willReturn(Optional.of(from));
+                    given(userRepository.findUserByUserId(toUser)).willReturn(Optional.of(to));
+
+                    //when
+                    FollowServiceResponse response = userService.followUser(followServiceRequest);
+
+                    // then
+                    assertThat(response.getFollowerCount()).isEqualTo(1);
+                    assertThat(response.isFollowing()).isTrue();
+
+                    verify(userRepository, times(1)).findUserByUserId(fromUser);
+                    verify(userRepository, times(1)).findUserByUserId(toUser);
+                }
+            }
+
+            @DisplayName("회원이 이미 팔로우한 회원은")
+            @Nested
+            class Context_FollowedUser {
+
+                @Test
+                @DisplayName("팔로우할 수 없다.")
+                void followUser_FollowedUser_400Exception() throws Exception {
+                    //given
+                    String fromUser = "testId";
+                    String toUser = "testId1";
+
+                    User from = UserFactory.user(1L, "testId");
+                    User to = UserFactory.user(2L, "testId1");
+
+                    FollowServiceRequest followServiceRequest = new FollowServiceRequest(fromUser,
+                            toUser);
+
+                    from.follow(to);
+
+                    given(userRepository.findUserByUserId(fromUser)).willReturn(Optional.of(from));
+                    given(userRepository.findUserByUserId(toUser)).willReturn(Optional.of(to));
+
+                    //when, then
+                    assertThatCode(() -> userService.followUser(followServiceRequest))
+                            .isInstanceOf(DuplicatedFollowException.class)
+                            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.BAD_REQUEST);
+
+                    verify(userRepository, times(1)).findUserByUserId(fromUser);
+                    verify(userRepository, times(1)).findUserByUserId(toUser);
+                }
+            }
+        }
+
+
     }
 
     private UserSignUpServiceRequest createSignUpRequest(String email) {
