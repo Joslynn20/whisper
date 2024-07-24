4d7723d08fd2444f5b6e381bb347b7402fb63820
Joslynn20 <joslynnkim@gmail.com>
2024-07-24 17:45:46 +0900

    test: 특정 회원의 팔로잉 회원 목록을 조회하는 findFollowingsOf 메소드 테스트

--- src/test/java/com/sns/whisper/unit/user/domain/UserRepositoryTest.java
+++ src/test/java/com/sns/whisper/unit/user/domain/UserRepositoryTest.java
@@ -8,11 +8,16 @@ import com.sns.whisper.domain.user.infrastructure.JPAUserRepository;
 import com.sns.whisper.global.config.JpaConfiguration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -22,6 +27,9 @@ public class UserRepositoryTest {
 
     @Autowired
     private JPAUserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     @Test
     @DisplayName("아이디가 이미 존재하는지 조회해온다.")
@@ -64,5 +72,75 @@ public class UserRepositoryTest {
         assertThat(savedUser).isNotNull();
         assertThat(savedUser.getUserId()).isEqualTo(userId);
         assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Nested
+    @DisplayName("findFollowingsOf 메소드는")
+    class Describe_findFollowingsOf {
+
+        @Nested
+        @DisplayName("특정 회원이 팔로잉한 회원이 없을 때")
+        class Context_NoFollowings {
+
+            @Test
+            @DisplayName("빈 리스트를 반환한다.")
+            void findFollowingsOf_NoFollowings_EmptyList() throws Exception {
+                //given
+                User user1 = UserFactory.createBasicUser("testId1");
+                User user2 = UserFactory.createBasicUser("testId2");
+                User user3 = UserFactory.createBasicUser("testId3");
+                User user4 = UserFactory.createBasicUser("testId4");
+
+                userRepository.saveAll(List.of(user1, user2, user3, user4));
+
+                user2.follow(user3);
+
+                testEntityManager.flush();
+                testEntityManager.clear();
+
+                //when
+                Pageable pageable = PageRequest.of(0, 3);
+                List<User> followings = userRepository.findFollowingsOf(user1, pageable);
+
+                //then
+                assertThat(followings).isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayName("특정 회원이 팔로잉한 회원이 있을 때,")
+        class Context_ValidFollowings {
+
+            @Test
+            @DisplayName("페이징 조건에 따라 회원의 following 목록을 조회할 수 있다.")
+            void findFollowingsOf_ValidFollowings_Pageable() throws Exception {
+                //given
+                User user1 = UserFactory.createBasicUser("testId1");
+                User user2 = UserFactory.createBasicUser("testId2");
+                User user3 = UserFactory.createBasicUser("testId3");
+                User user4 = UserFactory.createBasicUser("testId4");
+
+                userRepository.saveAll(List.of(user1, user2, user3, user4));
+
+                user2.follow(user1);
+                user3.follow(user1);
+                user4.follow(user1);
+
+                testEntityManager.flush();
+                testEntityManager.clear();
+
+                //when
+                Pageable pageable = PageRequest.of(0, 3);
+                List<User> followings = userRepository.findFollowingsOf(user1, pageable);
+
+                //then
+                assertThat(followings)
+                        .extracting("basicProfile")
+                        .extracting("userId")
+                        .containsExactly("testId2", "testId3", "testId4")
+                        .hasSize(3);
+            }
+        }
+
     }
 }
